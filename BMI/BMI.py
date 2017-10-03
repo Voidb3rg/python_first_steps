fb1483f234b4617757741f014673480aa9286544
Voidb3rg <32471640+Voidb3rg@users.noreply.github.com>
2017-10-03 04:39:04 +0200

    test
    abc

---
+++ BMI/BMI.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+''' main '''
+
+name=input('Name:')
+print('Hallo',name)
+groesse=input('Körpergröße:')
+
+bmi=0
+while True:
+    gewicht=input('Gewicht:')
+    if not gewicht:
+        break
+        bmi=round(int(gewicht)/(float(groesse)**2),2)
+        continue
+    print('BMI:',bmi)
+    if bmi >= 25:
+        print(name,', du hast Übergewicht')
+    elif bmi <=18.5:
+        print (name,', du hast Untergewicht')
+    else:
+        print(name,', du hast Normalgewicht')
+        
+print ('Ende des Programms')
+
+print ('jetzt aber')
